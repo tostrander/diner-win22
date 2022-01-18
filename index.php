209d427800c1ec11009341c03c5db0d2a88e5f13
Tina Ostrander <tostrander@greenriver.edu>
2022-01-18 17:21:25 -0500

    Created draft home page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//This is my CONTROLLER
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    //echo "<h1>My Diner</h1>";
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat-free
+$f3->run();
