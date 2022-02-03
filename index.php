7672b4b8c260c16874e7152593854b5b76005065
Tina Ostrander <tostrander@greenriver.edu>
2022-02-03 14:16:54 -0500

    Routes added to controller

--- index.php
+++ index.php
@@ -19,5 +19,37 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+//Define a breakfast route
+$f3->route('GET /breakfast', function() {
+    //echo "<h1>My Diner</h1>";
+
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+//Define a lunch route
+$f3->route('GET /lunch', function() {
+    //echo "<h1>My Diner</h1>";
+
+    $view = new Template();
+    echo $view->render('views/lunch-menu.html');
+});
+
+//Define a route for order 1
+$f3->route('GET /order1', function() {
+    //echo "<h1>Order 1 Form</h1>";
+
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+});
+
+//Define a route for order 2
+$f3->route('GET /order2', function() {
+    //echo "<h1>Order 1 Form</h1>";
+
+    $view = new Template();
+    echo $view->render('views/orderForm2.html');
+});
+
 //Run fat-free
 $f3->run();
