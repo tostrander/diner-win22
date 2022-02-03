80c149e2dd34fe1fe2290c77547f83e94f267771
Tina Ostrander <tostrander@greenriver.edu>
2022-02-03 17:03:07 -0500

    Add form processing

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 //This is my CONTROLLER
 
+//Turn on output buffering
+ob_start();
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Start the session
+session_start();
+var_dump($_SESSION);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
@@ -36,16 +43,41 @@ $f3->route('GET /lunch', function() {
 });
 
 //Define a route for order 1
-$f3->route('GET /order1', function() {
+$f3->route('GET|POST /order1', function($f3) {
     //echo "<h1>Order 1 Form</h1>";
+
+    //If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //TODO: Validate the data
+
+        //Add the data to the session variable
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+
+        //Redirect user to next page
+        $f3->reroute('order2');
+    }
 
     $view = new Template();
     echo $view->render('views/orderForm1.html');
 });
 
 //Define a route for order 2
-$f3->route('GET /order2', function() {
+$f3->route('GET|POST /order2', function($f3) {
     //echo "<h1>Order 1 Form</h1>";
+
+    //If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //TODO: Validate the data
+
+        //Add the data to the session variable
+        $_SESSION['conds'] = $_POST['conds'];
+
+        //Redirect user to summary page
+        $f3->reroute('summary');
+    }
 
     $view = new Template();
     echo $view->render('views/orderForm2.html');
@@ -53,3 +85,6 @@ $f3->route('GET /order2', function() {
 
 //Run fat-free
 $f3->run();
+
+//Send output to the browser
+ob_flush();
